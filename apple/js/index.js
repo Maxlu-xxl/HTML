68e590086465d4f38ca1bb6faa17ab6df7c5fcfb
Maxlu-xxl <13477806434@163.com>
2022-04-12 17:31:37 +0800

    备注

---
+++ apple/js/index.js
@@ -0,0 +1,50 @@
+var phone_buy = document.getElementById('phone_buy')
+phone_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/shop/buy-iphone/iphone-12"
+}
+
+var l_phone_pro_buy = document.getElementById('l_phone_pro_buy')
+l_phone_pro_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/shop/buy-iphone/iphone-12-pro"
+}
+
+var l_ipad_buy = document.getElementById('l_ipad_buy')
+l_ipad_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/cn-k12/shop/back-to-school"
+}
+
+
+// l_unit部分
+var l_unit_ipad_pro_buy = document.getElementById('l_unit_ipad_pro_buy')
+l_unit_ipad_pro_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/cn-k12/shop/buy-ipad/ipad-pro"
+}
+
+var l_unit_watch_buy = document.getElementById('l_unit_watch_buy')
+l_unit_watch_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/cn-k12/shop/buy-watch/apple-watch"
+}
+
+var l_unit_music_listen = document.getElementById('l_unit_music_listen')
+l_unit_music_listen.onclick = function() {
+    window.location.href = "https://music.apple.com/WebObjects/MZStore.woa/wa/viewMultiRoom?fcId=1561370672&app=music&ls=1&itscg=10000&itsct=rom_oasis_launch"
+}
+
+// 第二个ul
+var l_unit_iMac_buy = document.getElementById('l_unit_iMac_buy')
+l_unit_iMac_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/cn-k12/shop/buy-mac/imac"
+}
+
+
+var l_unit_Airtag_buy = document.getElementById('l_unit_Airtag_buy')
+l_unit_Airtag_buy.onclick = function() {
+    window.location.href = "#"
+}
+
+
+
+var l_unit_ios_look = document.getElementById('l_unit_ios_look')
+l_unit_ios_look.onclick = function() {
+    window.location.href = "#"
+}
---
+++ apple/js/test.js
@@ -0,0 +1,4 @@
+var phone_buy = document.getElementById('phone_buy')
+phone_buy.onclick = function() {
+    window.location.href = "https://www.apple.com.cn/shop/buy-iphone/iphone-12"
+}
